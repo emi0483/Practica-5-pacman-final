cbfc4d34131e0f82ed69b3a49e47ddd58aa8fac7
Danielgalvis2004 <103009862+Danielgalvis2004@users.noreply.github.com>
2023-05-27 22:09:03 -0500

    Update paredes.cpp

--- praactica_5/paredes.cpp
+++ praactica_5/paredes.cpp
@@ -1,20 +1,20 @@
 #include "paredes.h"
 
-void paredes::iniciar(int x_par, int y_par, int an_par, int al_par){
-    x=x_par;
-    y=y_par;
-    ancho=an_par;
-    alto=al_par;
+void paredes::iniciar(int x_par, int y_par, int an_par, int al_par){ //metodo de la clase paredes para coordenadas y dimensiones de una pared
+    x=x_par; //coordenada x
+    y=y_par; //coordenada y
+    ancho=an_par; //ancho
+    alto=al_par; //alto
 }
 
-bool paredes::contacto(int x_obj, int y_obj, int an_obj, int al_obj){
+bool paredes::contacto(int x_obj, int y_obj, int an_obj, int al_obj){ //metodo para saber si se colisiona con una pared
     bool temp;
-    for(int i=y;i<(y+alto);i++){
-        temp=(i>=y_obj && i<=(y_obj+al_obj));
-        for(int j=x;j<(x+ancho);j++){
+    for(int i=y;i<(y+alto);i++){ //pixeles que forman la pared
+        temp=(i>=y_obj && i<=(y_obj+al_obj)); //comparamos las iteraciones del for con las coordenadas del objeto
+        for(int j=x;j<(x+ancho);j++){ //lo mismo pero con ancho
             if(temp && j>=x_obj && j<=(x_obj+an_obj))
-                return 1;
+                return 1; //hay colision
         }
     }
-    return 0;
+    return 0; //no hay colision
 }
