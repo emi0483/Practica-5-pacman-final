41783e13ee050da2ebb3f5be3b9896cab095a677
Danielgalvis2004 <103009862+Danielgalvis2004@users.noreply.github.com>
2023-05-27 22:19:56 -0500

    Update moneda.cpp

--- praactica_5/moneda.cpp
+++ praactica_5/moneda.cpp
@@ -1,16 +1,17 @@
 #include "moneda.h"
 
-void moneda::aparecer(int x1, int y2){
-    x=x1;
-    y=y2;
-    mon= new QGraphicsPixmapItem;
-    mon->setPos(x,y);
-    mon->setPixmap(QPixmap(":/new/prefix1/imagenes/moneda.png").scaled(5,5));
+void moneda::aparecer(int x1, int y2){ //metodo para mostrar moneda en la posicion que queremos
+    x=x1; //coordenada x
+    y=y2; //coordenada y
+    mon= new QGraphicsPixmapItem; //creamos objeto
+    mon->setPos(x,y); //le damos la posicion
+    mon->setPixmap(QPixmap(":/new/prefix1/imagenes/moneda.png").scaled(5,5)); //cargamos la imagen y le damos 5x5 pixeles
 }
 
-bool moneda::comer(int obj_x, int obj_y){
-    if(x>=obj_x && x<=obj_x+30 && y>=obj_y && y<=obj_y+30){
-        delete mon;
-        return 1;}
-    return 0;
+bool moneda::comer(int obj_x, int obj_y){ //metodo pa comer moneda
+    //si las coordenadas xy de la moneda estan en el rango del objeto de 30pixeles
+    if(x>=obj_x && x<=obj_x+30 && y>=obj_y && y<=obj_y+30){ 
+        delete mon; //eliminamos moneda
+        return 1;} //moneda ha sido comida
+    return 0; //no se ha comido
 }
