cee611b6918868219ea56fe583da5a3287034af5
Danielgalvis2004 <103009862+Danielgalvis2004@users.noreply.github.com>
2023-05-27 22:25:42 -0500

    Update moneda.h

--- praactica_5/moneda.h
+++ praactica_5/moneda.h
@@ -4,8 +4,8 @@ class moneda
 {
 public:
     QGraphicsPixmapItem *mon;
-    int x,y;
-    void aparecer(int x1,int y2); //coordenadas moneda
+    int x,y; //coordenadas
+    void aparecer(int x1,int y2); //coordenadas moneda donde queremos
     bool comer(int obj_x, int obj_y); //coordenadas objeto
 };
 
