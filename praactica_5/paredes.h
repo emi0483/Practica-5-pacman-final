351299a58e1ecf24c58dd89089a6382a914ab62c
Danielgalvis2004 <103009862+Danielgalvis2004@users.noreply.github.com>
2023-05-27 22:11:46 -0500

    Update paredes.h

--- praactica_5/paredes.h
+++ praactica_5/paredes.h
@@ -5,6 +5,6 @@ class paredes
 public:
     void iniciar(int x_par,int y_par,int an_par,int al_par); //coordenadas y dimensiones pared
     bool contacto(int x_obj,int y_obj,int an_obj,int al_obj); //coordenadas y dimensiones objeto
-    int x,y,ancho,alto;
+    int x,y,ancho,alto; //para los for
 };
 
